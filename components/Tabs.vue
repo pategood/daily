7d20cae8abdaa83cfc324937a8e7f7ba59d14bee
蜀 松 <oncwnuMW0n21ZWJV2RXLzTnI2WJU@git.weixin.qq.com>
2021-05-31 11:57:28 +0800

    first commit

---
+++ components/Tabs.vue
@@ -0,0 +1,84 @@
+<template>
+	<view class="tabs">
+		<view class="tabs_title"  v-for="(item,index) in tabs" :key="index"
+			bindtap="handleItemTap" >
+				<view class='tabs_item'>{{item}}</view>
+				
+		</view>
+		<view class="tabs_content">
+			<!-- <image src="../static/img`/1.jpg" mode="widthFix"></image> -->
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "Tabs",
+		data() {
+			return {
+			};
+		},
+		props:{
+			tabs:{
+				type:Array,
+				// default: function () { return []}
+				default: ()=>[]
+			}
+		},
+		methods: {
+			handleItemTap(e) {
+				// 获取点击事件的索引
+				const {index} = e.currentTarget.dataset;
+				// 2 触发 父组件中的事件 自定义
+				this.triggerEvent("tabsItemChange", {index});
+			}
+		}
+		
+	}	
+</script>
+
+<style lang="scss">
+	.tabs{
+		height: 100%;
+		line-height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 0 10rpx 0 10rpx;
+		background-color: red;
+	}
+	.tabs_title{
+		width: 60%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: green;
+
+	}
+	.tabs_item{
+		width: fit-content;
+		margin: 20rpx auto;
+	}
+	
+	
+	
+	
+	
+	
+	
+/* 	.tabs{  //爷
+		// margin-left: ;
+	}
+	.tabs_title{  //父
+		height: 100rpx;
+		width:100rpx;
+		float: left;
+		background-color: red;
+	}
+	.tabs_item{  //子
+		height:100rpx;
+		line-height: 100rpx;
+		// text-align: center;
+	} */
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+	<view >
+		<Tabs :tabs='Tabs' />
+		<!-- <text>22</text> -->
+		
+		
+		
+		<swiper :indicator-dots="indicatorDots" :autoplay="true">
+			<swiper-item>
+				<view class="swiper-item">
+					
+				</view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	import Tabs from "../../components/Tabs.vue"
+	export default {
+		data() {
+			return {
+				Tabs:['精选','电视剧','综艺','电影', '动漫'],
+				indicatorDots: "true"
+			}
+		},
+		components:{
+			Tabs
+		},
+		
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
